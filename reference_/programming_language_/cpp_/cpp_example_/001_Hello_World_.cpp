d4c1c83135a0afc63d272bc8a64b010197034efb
kv9zy7 <githubkv9@gmail.com>
2024-06-09 20:36:26 +0100

    Add cpp_example Directory

---
+++ reference_/programming_language_/cpp_/cpp_example_/001_Hello_World_.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World" << endl;
+  cout << "I'm Nabil from Algeria" << endl;
+  cout << "jsjd sjdkjsh kdkkd sldk sl " << endl;
+
+  return 0;
+}
---
+++ reference_/programming_language_/cpp_/cpp_example_/003_if_statement_.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  if (true){
+    cout << "This is from --> if" << endl;
+  }
+  else if (false){
+    cout << "This is from --> else if" << endl;
+  }
+  else {
+    cout << "This is from --> else" << endl;
+  }
+}
