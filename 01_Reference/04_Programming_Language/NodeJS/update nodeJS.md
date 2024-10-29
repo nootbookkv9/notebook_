9dc6f0f36480f519e71ab6f97d2190c849e1de75
kv9 <githubkv9@gmail.com>
2024-10-29 09:57:43 +0100

    Some Updates

---
+++ 01_Reference/04_Programming_Language/NodeJS/update nodeJS.md
@@ -0,0 +1,9 @@
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
+nodejs --version
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+sudo n latest
+
+
---
+++ 01_Reference/06_Config/00_Setup_My_Computer/2_Install.bash
@@ -0,0 +1,18 @@
+sudo apt update
+
+sudo apt install ubuntu-restricted-extras -y
+sudo apt install ranger -y
+sudo apt install nodejs -y
+sudo apt install git -y
+sudo apt install calc -y
+sudo apt install vim -y
+sudo apt install neovim -y
+sudo apt install smplayer -y
+sudo apt install preload -y
+sudo apt install neofetch -y
+sudo apt install tree -y
+
+sudo add-apt-repository ppa:numix/ppa
+sudo apt-get update
+sudo apt-get install numix-gtk-theme numix-icon-theme-circle -y
+
